223f6eadef5cabd6713b3353e37e38f5cca76a04
Max Farafonov <farafonov.max@gmail.com>
2020-07-02 10:46:01 +0300

    Revise sublist

--- sublist/src/sublist_bench.clj
+++ sublist/src/sublist_bench.clj
@@ -4,10 +4,10 @@
 
 (defn- sublist?*
   ([list1 list2]
-  (cond
-    (= list1 (take (count list1) list2)) true
-    (empty? list2) false
-    :else (recur list1 (rest list2)))))
+   (cond
+     (= list1 (take (count list1) list2)) true
+     (empty? list2) false
+     :else (recur list1 (rest list2)))))
 
 (defn classify* [list1 list2]
   (let [lt (sublist?* list1 list2)
@@ -17,6 +17,15 @@
       lt :sublist
       gt :superlist
       :else :unequal)))
+
+(defn sublist?** [coll1 coll2]
+  (some #{coll1} (partition (count coll1) 1 coll2)))
+
+(defn classify** [coll1 coll2]
+  (cond (= coll1 coll2) :equal
+        (sublist?** coll1 coll2) :sublist
+        (sublist?** coll2 coll1) :superlist
+        :else :unequal))
 
 (def list1-cases
   [[]
@@ -58,3 +67,4 @@
 
 (defn bench [] (cc/quick-bench (dorun (map sublist/classify list1-cases list2-cases))))
 (defn bench* [] (cc/quick-bench (dorun (map classify* list1-cases list2-cases))))
+(defn bench** [] (cc/quick-bench (dorun (map classify** list1-cases list2-cases))))
