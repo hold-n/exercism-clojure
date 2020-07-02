e893d7f708459f5eee72ed6f98202c4f01dbd917
Max Farafonov <farafonov.max@gmail.com>
2020-07-02 12:39:48 +0300

    Revise isbn-verifier

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,31 +1,23 @@
 (ns isbn-verifier)
 
-(defn- parse-digits [isbn]
-  (if (re-matches #"\d{9}[\dX]" isbn)
-    isbn
-    nil))
+(defn- parse-num [^Character c]
+  (if (= c \X) 10 (Character/digit c 10)))
 
-(defn- parse-hyphens [isbn]
-  (if (re-matches #"\d-\d{3}-\d{5}-[\dX]" isbn)
-    (remove #{\-} isbn)
-    nil))
+(defn- isbn10->nums [isbn]
+  (when (re-matches #"\d{9}[\dX]|\d-\d{3}-\d{5}-[\dX]" isbn)
+    (map parse-num (remove #{\-} isbn))))
 
-(defn- parse-digit [^Character d]
-  (if (= d \X) 10 (Character/digit d 10)))
+(defn- checksum10? [nums]
+  (let [divisible-by? #(zero? (rem %2 %1))]
+    (->> nums
+         (map * (range 10 0 -1))
+         (reduce +)
+         (divisible-by? 11))))
 
-(defn- valid? [digits]
-  (->> digits
-       (map vector [10 9 8 7 6 5 4 3 2 1])
-       (map #(reduce * %))
-       (reduce +)
-       (#(rem % 11))
-       zero?))
-
-(defn isbn? [isbn]
-  (let [chars (or
-                (parse-digits isbn)
-                (parse-hyphens isbn))
-        digits (map parse-digit chars)]
+(defn- isbn10? [isbn]
+  (let [nums (isbn10->nums isbn)]
     (true? (and
-            (seq digits)
-            (valid? digits)))))
+            (seq nums)
+            (checksum10? nums)))))
+
+(def isbn? isbn10?)
